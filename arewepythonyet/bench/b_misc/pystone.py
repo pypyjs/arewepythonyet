05b17bf73346702485d79dc112077a17030b6496
Ryan Kelly <ryan@rfk.id.au>
2015-01-14 17:09:00 +1100

    Do just a single "pystone" benchmark, covering both previous cases.

--- arewepythonyet/bench/b_misc/pystone.py
+++ arewepythonyet/bench/b_misc/pystone.py
@@ -3,5 +3,6 @@ from test import pystone
 # Without this, d8 produces zero pystones per second due to warmup
 # interfering with the way pystone tries to adjust for null loop time.
 pystone.pystones(1)
-_, stones = pystone.pystones(50000)
-print stones
+for _ in xrange(10):
+    _, stones = pystone.pystones(50000)
+    print stones
--- arewepythonyet/bench/b_misc/pystone_warm.py
+++
@@ -1,4 +0,0 @@
-from test import pystone
-for _ in xrange(10):
-    _, stones = pystone.pystones(50000)
-    print stones
