c0d051411b97ce80455f013ea1e45a5dbe1a06a5
Ryan Kelly <ryan@rfk.id.au>
2015-01-18 12:55:20 +1100

    Refactor graph rendering to keep more state directly in the page.

--- website/js/common.js
+++ website/js/common.js
@@ -1,35 +1,25 @@
 var AWPY = {};
 
-AWPY.Graph = function Graph(options) {
-  this.options = options;
-  this.config = options.config || {};
-  delete options.config;
-};
+AWPY.data_promises = {};
 
-
-AWPY.Graph.prototype.draw = function() {
-  var opts = {};
-  for (var k in this.options) {
-    if (typeof this.options[k] === "function") {
-      opts[k] = this.options[k].call(this);
-    } else {
-      opts[k] = this.options[k];
-    }
+AWPY.fetch = function(name) {
+  if (!name) {
+    return Promise.resolve();
   }
-  if (!opts.width) {
-    opts.width = $(opts.target).width();
+  if (!AWPY.data_promises[name]) {
+    AWPY.data_promises[name] = new Promise(function(resolve, reject) {
+      d3.json(name, function(data) {
+        if (data) {
+          AWPY.convert_timestamps(data);
+          resolve(data);
+        } else {
+          reject("failed to fetch " + name);
+        }
+      });
+    });
   }
-  if (!opts.height) {
-    opts.height = 250;
-  }
-  // Re-rendering bar charts doesn't seem to work quite right.
-  // This forces it to re-draw from scratch.
-  if (opts.chart_type === "bar") {
-    $(opts.target).empty();
-  }
-  MG.data_graphic(opts);
+  return AWPY.data_promises[name];
 }
-
 
 AWPY.convert_timestamps = function convert_timestamps(data) {
   for (var k in data) {
@@ -40,3 +30,88 @@ AWPY.convert_timestamps = function convert_timestamps(data) {
     }
   }
 }
+
+
+AWPY.all_graphs = [];
+
+AWPY.draw_all_the_graphs = function draw_all_the_graphs() {
+  var p = Promise.resolve();
+  for (var i = 0; i < AWPY.all_graphs.length; i++) {
+    p = p.then((function(i) { return function() {
+      return AWPY.all_graphs[i].draw();
+    };})(i));
+  }
+}
+
+
+AWPY.from_location_var = function from_location_var(name) {
+  var r = new RegExp("(#|&)" + name + "=([a-zA-Z0-9_\\-\\+]+)($|&)");
+  var match = r.exec(window.location.hash);
+  return match ? match[2] : undefined;
+}
+
+
+AWPY.to_location_var = function to_location_var(name, value) {
+  var r = new RegExp("(#|&)" + name + "=([a-zA-Z0-9_\\-\\+]+)($|&)");
+  var hash = window.location.hash;
+  var match = r.exec(hash);
+  if (!match) {
+    if (hash && hash !== "#") {
+      window.location.hash += "&" + name + "=" + value;
+    } else {
+      window.location.hash = "#" + name + "=" + value;
+    }
+  } else {
+    var prefix = hash.substr(0, match.index + 1);
+    var suffix = hash.substr(match.index + match[0].length - match[3].length);
+    window.location.hash = prefix + name + "=" + value + suffix;
+  }
+}
+
+
+AWPY.Graph = function Graph(options) {
+  this.options = options;
+  AWPY.all_graphs.push(this);
+};
+
+
+AWPY.Graph.prototype.draw = function() {
+  var opts = {};
+  // Gather all the graph options.
+  // Any functions return promises that we need to resolve async.
+  var p = Promise.resolve();
+  for (var k in this.options) {
+    if (typeof this.options[k] !== "function") {
+      opts[k] = this.options[k];
+    } else {
+      p = p.then((function(k) { return (function() {
+        return this.options[k].call(this);
+      }).bind(this);}).bind(this)(k))
+      .then((function(k) { return function(val) {
+        opts[k] = val;
+      };})(k));
+    }
+  }
+  return p.then((function() {
+    if (!opts.width) {
+      opts.width = $(opts.target).width();
+    }
+    if (!opts.height) {
+      opts.height = 250;
+    }
+    // Re-rendering bar charts doesn't seem to work quite right.
+    // This forces it to re-draw from scratch.
+    if (opts.chart_type === "bar") {
+      $(opts.target).empty();
+    }
+    MG.data_graphic(opts);
+  }).bind(this));
+}
+
+$(document).ready(function() {
+
+  $(window).on('resize', function() {
+    AWPY.draw_all_the_graphs();
+  });
+
+});
--- website/js/overview.js
+++ website/js/overview.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
 
-  var summary = null;
-
   // The latest results of the python benchmarks, normalized to native.
+
+  function cfg_norm(val) {
+    if (typeof val !== "undefined") {
+      $("#config-py-norm").val(val);
+      $("#config-py-norm").trigger("change");
+    }
+    var norm = $("#config-py-norm").val();
+    if (norm === "pypy" && !cfg_jit()) {
+      norm = "pypy-nojit";
+    }
+    return norm;
+  }
+
+  function cfg_jit(val) {
+    if (typeof val !== "undefined") {
+      $("#config-py-jit").val(val ? "on" : "off");
+      $("#config-py-jit").trigger("change");
+    }
+    return $("#config-py-jit").val() === "on";
+  }
+
+  function cfg_js_engines() {
+    if (cfg_jit()) {
+      js_engines = ["js+pypy", "d8+pypy"];
+    } else {
+      js_engines = ["js+pypy-nojit", "d8+pypy-nojit"];
+    }
+    return js_engines;
+  }
 
   var pyBenchBreakdown = new AWPY.Graph({
     title: "Individual benchmarks",
-    config: {
-      norm: "python",
-      jit: true
-    },
     description: function() {
       desc = "Mean time for each python benchmark,";
-      desc += " normalized to " + this.config.norm;
+      desc += " normalized to " + cfg_norm();
       return desc
     },
     chart_type: "bar",
@@ -20,50 +43,48 @@ $(document).ready(function() {
     x_accessor: "value",
     y_accessor: "label",
     x_label: function() {
-      return "runtime (normalized to " + this.config.norm + ")";
+      return "runtime (normalized to " + cfg_norm() + ")";
     },
     baseline_accessor: "baseline",
     data: function() {
-      var data = [];
-      var product = 1;
-      BENCHMARKS: for (var b_name in summary.py.benchmarks) {
-        var b_res = summary.py.benchmarks[b_name];
-        var js_engines;
-        if (this.config.jit) {
-          js_engines = ["js+pypy", "d8+pypy"];
-        } else {
-          js_engines = ["js+pypy-nojit", "d8+pypy-nojit"];
-        }
-        if (!b_res[0].engines[this.config.norm]) continue;
-        for (var i = 0; i < js_engines.length; i++) {
-          if (!b_res[0].engines[js_engines[i]]) {
-            continue BENCHMARKS;
+      return AWPY.fetch("data/summary.json").then((function(summary) {
+        var data = [];
+        var product = 1;
+        var norm = cfg_norm();
+        var js_engines = cfg_js_engines();
+        BENCHMARKS: for (var b_name in summary.py.benchmarks) {
+          var b_res = summary.py.benchmarks[b_name];
+          if (!b_res[0].engines[norm]) continue;
+          for (var i = 0; i < js_engines.length; i++) {
+            if (!b_res[0].engines[js_engines[i]]) {
+              continue BENCHMARKS;
+            }
           }
+          var b_norm = b_res[0].engines[norm].mean;
+          var b_value = 0;
+          for (var i = 0; i < js_engines.length; i++) {
+            b_value += b_res[0].engines[js_engines[i]].mean;
+          }
+          b_value = (b_value / js_engines.length) / b_norm;
+          data.push({
+            label: b_name,
+            value: b_value,
+            baseline: 1,
+          });
+          product = product * b_value;
         }
-        var b_norm = b_res[0].engines[this.config.norm].mean;
-        var b_value = 0;
-        for (var i = 0; i < js_engines.length; i++) {
-          b_value += b_res[0].engines[js_engines[i]].mean;
+        var geo_mean = Math.pow(product, 1 / data.length);
+        var txt;
+        if (geo_mean >= 1) {
+          txt = Math.round(geo_mean * 10) / 10;
+          txt += " times slower than " + norm;
+        } else {
+          txt = Math.round((1 / geo_mean) * 10) / 10;
+          txt += " times faster than " + norm;
         }
-        b_value = (b_value / js_engines.length) / b_norm;
-        data.push({
-          label: b_name,
-          value: b_value,
-          baseline: 1,
-        });
-        product = product * b_value;
-      }
-      var geo_mean = Math.pow(product, 1 / data.length);
-      var txt;
-      if (geo_mean >= 1) {
-        txt = Math.round(geo_mean * 10) / 10;
-        txt += " times slower than " + this.config.norm;
-      } else {
-        txt = Math.round((1 / geo_mean) * 10) / 10;
-        txt += " times faster than " + this.config.norm;
-      }
-      $("#compare-py-trend").text(txt);
-      return data;
+        $("#compare-py-trend").text(txt);
+        return data;
+      }).bind(this))
     }
   });
 
@@ -77,14 +98,14 @@ $(document).ready(function() {
     },
     description: function() {
       desc = "Mean time across all benchmarks, over time, ";
-      desc += "normalized to " + this.config.norm;
+      desc += "normalized to " + cfg_norm();
       return desc
     },
     legend: function() {
-      if (this.config.jit) {
-        return [this.config.norm, "js+pypy", "d8+pypy"];
+      if (cfg_jit()) {
+        return [cfg_norm(), "js+pypy", "d8+pypy"];
       } else {
-        return [this.config.norm, "js+pypy-nojit", "d8+pypy-nojit"];
+        return [cfg_norm(), "js+pypy-nojit", "d8+pypy-nojit"];
       }
     },
     target: "#graph-py-trend",
@@ -92,61 +113,54 @@ $(document).ready(function() {
     x_accessor: "timestamp",
     y_accessor: "value",
     y_label: function() {
-      return "runtime (normalized to " + this.config.norm + ")";
+      return "runtime (normalized to " + cfg_norm() + ")";
     },
     data: function() {
-      var b_means = summary.py.geometric_mean;
-      var b_means_norm = {};
-      for (var i = b_means.length - 1; i >= 0; i--) {
-        var norm = b_means[i].engines[this.config.norm].value;
-        for (var e_name in b_means[i].engines) {
-          var e_means_norm = b_means_norm[e_name];
-          if (!e_means_norm) {
-            e_means_norm = b_means_norm[e_name] = [];
+      return AWPY.fetch("data/summary.json").then((function(summary) {
+        var norm = cfg_norm();
+        var b_means = summary.py.geometric_mean;
+        var b_means_norm = {};
+        for (var i = b_means.length - 1; i >= 0; i--) {
+          var b_norm = b_means[i].engines[norm].value;
+          for (var e_name in b_means[i].engines) {
+            var e_means_norm = b_means_norm[e_name];
+            if (!e_means_norm) {
+              e_means_norm = b_means_norm[e_name] = [];
+            }
+            e_means_norm.push({
+              timestamp: b_means[i].timestamp,
+              value: b_means[i].engines[e_name].value / b_norm
+            });
           }
-          e_means_norm.push({
-            timestamp: b_means[i].timestamp,
-            value: b_means[i].engines[e_name].value / norm
-          });
         }
-      }
-      return [
-        b_means_norm[this.config.norm],
-        b_means_norm["js+pypy" + (this.config.jit ? "" : "-nojit")],
-        b_means_norm["d8+pypy" + (this.config.jit ? "" : "-nojit")]
-      ]
+        return [
+          b_means_norm[norm],
+          b_means_norm["js+pypy" + (cfg_jit() ? "" : "-nojit")],
+          b_means_norm["d8+pypy" + (cfg_jit() ? "" : "-nojit")]
+        ]
+      }).bind(this));
     }
   });
 
   $("#config-py-norm").on("change", function() {
-    var norm = $(this).val();
-    if (norm == "pypy" && $("#config-py-jit").val() === "off") {
-      norm = "pypy-nojit";
-    }
-    pyBenchBreakdown.config.norm = norm;
-    pyBenchMeanTrend.config.norm = norm;
-    if (summary) {
-      pyBenchBreakdown.draw();
-      pyBenchMeanTrend.draw();
-    }
+    AWPY.to_location_var("norm", cfg_norm());
+    pyBenchBreakdown.draw();
+    pyBenchMeanTrend.draw();
   });
-  $("#config-py-norm").trigger("change");
 
   $("#config-py-jit").on("change", function() {
-    var jit = $(this).val() === "on";
-    pyBenchBreakdown.config.jit = jit;
-    pyBenchMeanTrend.config.jit = jit;
-    $("#config-py-norm").trigger("change");
+    var val = cfg_jit() ? "on" : "off";
+    AWPY.to_location_var("jit", val);
+    if ($("#config-startup-jit").val() !== val) {
+      $("#config-startup-jit").val(val);
+    }
+    AWPY.draw_all_the_graphs();
   });
-  $("#config-py-jit").trigger("change");
 
 
   var miscDownloadSize = new AWPY.Graph({
     title: "Download Size",
     description: "Total size of all core interpreter files, over time",
-    config: {
-      jit: true
-    },
     target: "#graph-file-size",
     x_accessor: "timestamp",
     y_accessor: "value",
@@ -154,33 +168,32 @@ $(document).ready(function() {
     legend: ["raw", "gz"],
     legend_target: "#legend-file-size",
     data: function() {
-      var data_raw = [];
-      var engine = "pypy" + (this.config.jit ? "" : "-nojit");
-      var results = summary["misc"]["file_size_raw"];
-      for (var i = results.length - 1; i >= 0; i--) {
-        data_raw.push({
-          "timestamp": results[i]["timestamp"],
-          "value": results[i].engines[engine].mean,
-        });
-      }
-      var data_gz = [];
-      var results = summary["misc"]["file_size_gz"];
-      for (var i = results.length - 1; i >= 0; i--) {
-        data_gz.push({
-          "timestamp": results[i]["timestamp"],
-          "value": results[i].engines[engine].mean,
-        });
-      }
-      return [data_raw, data_gz];
+      return AWPY.fetch("data/summary.json").then((function(summary) {
+        var data_raw = [];
+        var engine = "pypy" + (cfg_jit() ? "" : "-nojit");
+        var results = summary["misc"]["file_size_raw"];
+        for (var i = results.length - 1; i >= 0; i--) {
+          data_raw.push({
+            "timestamp": results[i]["timestamp"],
+            "value": results[i].engines[engine].mean,
+          });
+        }
+        var data_gz = [];
+        var results = summary["misc"]["file_size_gz"];
+        for (var i = results.length - 1; i >= 0; i--) {
+          data_gz.push({
+            "timestamp": results[i]["timestamp"],
+            "value": results[i].engines[engine].mean,
+          });
+        }
+        return [data_raw, data_gz];
+      }).bind(this));
     }
   });
 
   var miscLoadTime = new AWPY.Graph({
     title: "Load Time",
     description: "Time to load and initialize the core interpreter, over time",
-    config: {
-      jit: true
-    },
     target: "#graph-load-time",
     legend: ["js+pypy", "d8+pypy"],
     legend_target: "#legend-load-time",
@@ -188,54 +201,37 @@ $(document).ready(function() {
     y_accessor: "value",
     y_label: "load time (seconds)",
     data: function() {
-      var engine = "pypy" + (this.config.jit ? "" : "-nojit");
-      var data_js = [];
-      var results = summary["misc"]["load_time"];
-      for (var i = results.length - 1; i >= 0; i--) {
-        data_js.push({
-          "timestamp": results[i]["timestamp"],
-          "value": results[i].engines["js+" + engine].mean,
-        });
-      }
-      var data_d8 = [];
-      var results = summary["misc"]["load_time"];
-      for (var i = results.length - 1; i >= 0; i--) {
-        data_d8.push({
-          "timestamp": results[i]["timestamp"],
-          "value": results[i].engines["d8+" + engine].mean,
-        });
-      }
-      return [data_js, data_d8];
+      return AWPY.fetch("data/summary.json").then((function(summary) {
+        var engine = "pypy" + (cfg_jit() ? "" : "-nojit");
+        var data_js = [];
+        var results = summary["misc"]["load_time"];
+        for (var i = results.length - 1; i >= 0; i--) {
+          data_js.push({
+            "timestamp": results[i]["timestamp"],
+            "value": results[i].engines["js+" + engine].mean,
+          });
+        }
+        var data_d8 = [];
+        var results = summary["misc"]["load_time"];
+        for (var i = results.length - 1; i >= 0; i--) {
+          data_d8.push({
+            "timestamp": results[i]["timestamp"],
+            "value": results[i].engines["d8+" + engine].mean,
+          });
+        }
+        return [data_js, data_d8];
+      }).bind(this));
     }
   });
 
   $("#config-startup-jit").on("change", function() {
-    var jit = $(this).val() === "on";
-    miscDownloadSize.config.jit = jit;
-    miscLoadTime.config.jit = jit;
-    if (summary) {
-      miscDownloadSize.draw();
-      miscLoadTime.draw();
-    }
-  });
-  $("#config-startup-jit").trigger("change");
-
-  function redrawAllTheGraphs() {
-    if (!summary) return;
-    pyBenchBreakdown.draw();
-    pyBenchMeanTrend.draw();
-    miscDownloadSize.draw();
-    miscLoadTime.draw();
-  }
-
-  d3.json("/data/summary.json", function(data) {
-    AWPY.convert_timestamps(data);
-    summary = data;
-    redrawAllTheGraphs();
+    $("#config-py-jit").val($(this).val());
+    $("#config-py-jit").trigger("change");
   });
 
-  $(window).on('resize', function() {
-    redrawAllTheGraphs();
-  });
+  cfg_norm(AWPY.from_location_var("norm"));
+  cfg_jit(AWPY.from_location_var("jit") == "on" ? true : false);
+
+  AWPY.draw_all_the_graphs();
 
 });
