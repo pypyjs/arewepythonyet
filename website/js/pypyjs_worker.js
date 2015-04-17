9bce233761dd977a8a3a063b6dad3a0eea5f447b
Ryan Kelly <ryan@rfk.id.au>
2015-04-18 09:30:12 +1000

    Fix path to pypyjs.org vm javascript

--- website/js/pypyjs_worker.js
+++ website/js/pypyjs_worker.js
@@ -1,6 +1,6 @@
 var t_start = +Date.now();
 postMessage({ t_start: t_start });
-importScripts("http://pypyjs.org/demo/lib/pypy.js");
+importScripts("http://pypyjs.org/js/pypy.js-0.2.0/lib/pypy.js");
 var vm = new PyPyJS();
 vm.ready.then(function() {
   var t_end = +Date.now();
