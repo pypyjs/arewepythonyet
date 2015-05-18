54e2795c5a70834462b6d8a159bb3bf5b9674f2a
Ryan Kelly <ryan@rfk.id.au>
2015-05-19 08:22:04 +1000

    Add more benchmark runs

--- arewepythonyet/bench/runner.js
+++ arewepythonyet/bench/runner.js
@@ -3,9 +3,7 @@ var vm = new PyPyJS();
 vm.ready.then(function() {
   return vm.loadModuleData.apply(vm, {{py_imports}})
 }).then(function() {
-  return vm.eval("__name__ = '__main__'")
-}).then(function() {
-  return vm.eval({{py_code}})
+  return vm.exec({{py_code}})
 }).catch(function(err) {
   printErr(err);
   throw err;
